3350839a8050262a216eb38cec903829328fe460
Jegor777 <fortnajtdobran@gmail.com>
2023-01-07 12:52:49 +0200

    start js

---
+++ js/index.js
@@ -0,0 +1,69 @@
+'use strict';
+// 'Java script'.length
+
+//  let myFriendName = "Poly";
+// //  console.log(number)
+
+// let number = 10;
+
+// console.log(1);
+// console.log('hi');
+// console.log(number);
+
+// number = 15;
+// console.log(number);
+
+
+
+
+// const userName = '10px';
+// console.log(userName);
+
+// const isOnline = true;
+// console.log(isOnline);
+
+// console.log(undefined);
+// console.log(null);
+// console.log(NaN);
+
+
+// const myName = 'Egor';
+// console.log(myName);
+
+// const number = 6;
+// console.log(number);
+
+// const isActive = true;
+// console.log(isActive);
+
+// const age = 10 , 4 ;
+
+// console.log(age);
+
+// const age = '10.25 , 56' ;
+// console.log(age)
+// const message = 'test';
+
+
+// //  const userAnswer = confirm(message);
+// //  console.log(userAnswer);
+// // alert('Heloo');
+
+//  const orderQuality = prompt('How much sugar?');
+
+ 
+//  console.log(typeof orderQuality);
+
+// const answer = prompt('Чи хочете ви отримати підписку');
+// console.log(answer);
+
+// console.log(7 % 3);
+
+// let sum = 10 / 2;
+// sum += 5;
+// console.log(5 > 2);
+const a = 5;
+const b = 10;
+const c = '5';
+const d = 'Heloo'
+console.log(a !== b);
