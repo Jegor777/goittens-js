c69ec9d77b8eb6e1792a891f6b88a3b2b6fcae13
Jegor777 <fortnajtdobran@gmail.com>
2023-01-14 13:02:29 +0200

    first

---
+++ js/numbers.js
@@ -0,0 +1,61 @@
+const message = ' цьому рядку 26 символів ';
+
+console.log(message.includes('цьому') ) ;
+
+// console.log(message.toLocaleUpperCase());
+// console.log(message.length);
+
+// const firstName = 'Mango'
+// const lastName = 'Lee'
+// const fullName = firstName + ' ' + 'Lee';
+// console.log(fullName);
+
+
+// const firstName = 'Mango'
+// const lastName = 'Lee'
+// const room = 716;
+// const type = 'VIP';
+// const fullName = `Гість + ' ' + lastName + ' поселеяється ' + room + ' '+ 'номер' ${type}`;
+// console.log(fullName);
+
+
+
+const quality = 15;
+const orderMsg = `Ви замовляєте ${quality} тістечок`;
+console.log(orderMsg);
+
+
+
+// const blacklistedWord1 = 'спам';
+// const blacklistedWord2 = 'розпродаж';
+
+// const string1 = 'Привіт, я принц Абдул, це не спам, пропоную тобі мільйон!';
+// const string2 = 'Найбільший РОЗПРОДАЖ цього тижня, не пропустіть!';
+// const string3 = 'Рекламна кампанія #jsIsAwesome';
+
+//  const norm=string1.toLocaleLowerCase();
+//  const norm2=string2.toLocaleLowerCase();
+//  const norm3=string3.toLocaleLowerCase();
+
+
+//  console.log(norm.includes(blacklistedWord1));
+//  console.log(norm2.includes(blacklistedWord2));
+//  console.log(norm.includes(blacklistedWord1))
+//  console.log(norm2.includes(blacklistedWord2))
+//  console.log(norm3.includes(blacklistedWord1))
+
+ console.log(5 && 4 && 0 && true && 1);
+
+
+ console.log(false || 5 || null);
+//  console.log(5 && 2);
+console.log(!false);
+
+
+
+
+
+
+
+
+
