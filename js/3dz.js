c0547a09bb1968e59f83d539be323a1f893780df
Jegor777 <fortnajtdobran@gmail.com>
2023-01-27 23:37:32 +0200

    first comit

---
+++ js/3dz.js
@@ -0,0 +1,37 @@
+//1//
+// const number = prompt('Type your number');
+// if(number>55 && number <100){
+//     console.log("Число потрапляє в діапазон");
+// }
+// else{
+//     console.log("Не потрапляє в діапозон");
+// }
+
+const maxim = 55;
+if(maxim>0 && maxim <16){
+    console.log('Діти');
+}
+else if(maxim>17 && maxim <60){
+    console.log('Дорослі')
+}
+else if(maxim>61 && maxim <100){
+    console.log('пенсіонер')
+}
+// practice //
+
+
+const salary = 2500;
+if(salary < 1000){
+    console.log('Level 1');
+}
+else if(salary > 2000 && salary < 3000) {
+console.log('Level 2')
+}else if(salary > 3000 && salary < 4000) {
+    console.log('Level 3')
+}
+else{
+    console.log('Ви не наш робітник');
+}
+
+
+
