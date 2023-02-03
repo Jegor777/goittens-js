4d88b52ab1f916c3354fc9425baa3456e607a0af
Jegor777 <fortnajtdobran@gmail.com>
2023-02-03 14:22:25 +0200

    xdcxc

---
+++ js/4hw.js
@@ -0,0 +1,25 @@
+for (let i = 1; i <= 10; i += 1  ) {
+    console.log(i);
+    
+}
+
+
+
+const min = 1;
+const max = 10;
+let total = 0;
+for (let i = min;i <=max; i  += 1) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+    
+}
+
+
+
+let num = 20;
+
+while (num < 28) {
+    num += 1;
+    console.log(num);
+}
