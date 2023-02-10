bc721ed98184f6c70c500d6bc5fb4cd9bf1a91dc
Jegor777 <fortnajtdobran@gmail.com>
2023-02-10 16:00:15 +0200

    do 1/3 of hw

---
+++ js/5hw.js
@@ -0,0 +1,19 @@
+//1
+const arr1 = [1, 5, '4', 'hello' ];
+const arr2 = [ true,2, {}, ['a'], 222 ];
+console.log(arr1[1]);
+console.log(arr2[1]);
+console.log(arr1[1] + arr2[1]);
+
+//2
+
+
+//3
+for (const elements of arr2)  {
+    console.log(elements);
+}
+//4
+const message = 'Welcome to Ukraine!'
+
+//5
+const styles = ['Джаз' , 'Блюз']
